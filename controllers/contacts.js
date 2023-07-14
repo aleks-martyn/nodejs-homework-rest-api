5f65679958b9aa942551586fd83f3dd138b7c79d
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-07-14 19:20:34 +0300

    Update contacts.js

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,14 +1,6 @@
-const Joi = require("joi");
-
 const contacts = require("../models/contacts");
 
 const { HttpError, CtrlWrapper } = require("../helpers");
-
-const addSchema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().required(),
-  phone: Joi.string().required(),
-});
 
 const getAll = async (req, res) => {
   res.json(await contacts.listContacts());
@@ -26,23 +18,11 @@ const getById = async (req, res) => {
 };
 
 const add = async (req, res) => {
-  const { error } = addSchema.validate(req.body);
-
-  if (error) {
-    throw HttpError(400, error.message);
-  }
-
   const result = await contacts.addContact(req.body);
   res.status(201).json(result);
 };
 
 const updateById = async (req, res) => {
-  const { error } = addSchema.validate(req.body);
-
-  if (error) {
-    throw HttpError(400, error.message);
-  }
-
   const { contactId } = req.params;
   const result = await contacts.updateContact(contactId, req.body);
 
