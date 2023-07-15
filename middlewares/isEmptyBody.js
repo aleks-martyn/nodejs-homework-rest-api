f34927a684c672cf950891acef34ddeef4f3d926
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-07-15 21:55:08 +0300

    Update isEmptyBody.js

--- middlewares/isEmptyBody.js
+++ middlewares/isEmptyBody.js
@@ -2,9 +2,11 @@ import { HttpError } from "../helpers/index.js";
 
 const isEmptyBody = (req, res, next) => {
   const { length } = Object.keys(req.body);
+
   if (!length) {
     next(HttpError(400, "missing fields"));
   }
   next();
 };
+
 export default isEmptyBody;
