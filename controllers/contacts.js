82d54cf60c30891e89e7c6a815e273fabb4f4237
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-07-15 21:28:28 +0300

    Update contacts.js

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,14 +1,16 @@
-const contacts = require("../models/contacts");
+import contactsService from "../models/contacts.js";
 
-const { HttpError, CtrlWrapper } = require("../helpers");
+import { ctrlWrapper } from "../decorators/index.js";
+
+import { HttpError } from "../helpers/index.js";
 
 const getAll = async (req, res) => {
-  res.json(await contacts.listContacts());
+  res.json(await contactsService.listContacts());
 };
 
 const getById = async (req, res) => {
   const { contactId } = req.params;
-  const result = await contacts.getContactById(contactId);
+  const result = await contactsService.getContactById(contactId);
 
   if (!result) {
     throw HttpError(404, "Not found");
@@ -18,13 +20,13 @@ const getById = async (req, res) => {
 };
 
 const add = async (req, res) => {
-  const result = await contacts.addContact(req.body);
+  const result = await contactsService.addContact(req.body);
   res.status(201).json(result);
 };
 
 const updateById = async (req, res) => {
   const { contactId } = req.params;
-  const result = await contacts.updateContact(contactId, req.body);
+  const result = await contactsService.updateContact(contactId, req.body);
 
   if (!result) {
     throw HttpError(404, "Not found");
@@ -35,7 +37,7 @@ const updateById = async (req, res) => {
 
 const deleteById = async (req, res) => {
   const { contactId } = req.params;
-  const result = await contacts.removeContact(contactId);
+  const result = await contactsService.removeContact(contactId);
 
   if (!result) {
     throw HttpError(404, "Not found");
@@ -44,10 +46,10 @@ const deleteById = async (req, res) => {
   res.json({ message: "contact deleted" });
 };
 
-module.exports = {
-  getAll: CtrlWrapper(getAll),
-  getById: CtrlWrapper(getById),
-  add: CtrlWrapper(add),
-  updateById: CtrlWrapper(updateById),
-  deleteById: CtrlWrapper(deleteById),
+export default {
+  getAll: ctrlWrapper(getAll),
+  getById: ctrlWrapper(getById),
+  add: ctrlWrapper(add),
+  updateById: ctrlWrapper(updateById),
+  deleteById: ctrlWrapper(deleteById),
 };
