9f7543b3b842058167d3c9afa209da1e15a247af
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-07-15 21:50:39 +0300

    Update contacts.js

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,7 +2,9 @@ import express from "express";
 
 import contactsController from "../../controllers/contacts.js";
 
-import { validateBody }  from "../../middlewares/index.js";
+import { validateBody } from "../../middlewares/index.js";
+
+import { isEmptyBody } from "../../middlewares/index.js";
 
 import schemas from "../../schemas/contacts.js";
 
@@ -12,9 +14,19 @@ contactsRouter.get("/", contactsController.getAll);
 
 contactsRouter.get("/:contactId", contactsController.getById);
 
-contactsRouter.post("/", validateBody(schemas.addSchema), contactsController.add);
+contactsRouter.post(
+  "/",
+  isEmptyBody,
+  validateBody(schemas.addSchema),
+  contactsController.add
+);
 
-contactsRouter.put("/:contactId", validateBody(schemas.addSchema), contactsController.updateById);
+contactsRouter.put(
+  "/:contactId",
+  isEmptyBody,
+  validateBody(schemas.addSchema),
+  contactsController.updateById
+);
 
 contactsRouter.delete("/:contactId", contactsController.deleteById);
 
