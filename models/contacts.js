05cd3b6b6f43f8ed01e10137ced137ca477ac04f
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-07-15 21:29:29 +0300

    Update contacts.js

--- models/contacts.js
+++ models/contacts.js
@@ -1,8 +1,8 @@
-const fs = require("fs/promises");
-const path = require("path");
-const { nanoid } = require("nanoid");
+import fs from "fs/promises";
+import path from "path";
+import { nanoid } from "nanoid";
 
-const contactsPath = path.join(__dirname, "contacts.json");
+const contactsPath = path.resolve("models", "contacts.json");
 
 const listContacts = async () => {
   const contacts = await fs.readFile(contactsPath, "utf-8");
@@ -58,7 +58,7 @@ const updateContact = async (contactId, body) => {
   return contacts[contactIndex];
 };
 
-module.exports = {
+export default {
   listContacts,
   getContactById,
   removeContact,
