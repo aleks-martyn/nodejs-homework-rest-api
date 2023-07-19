23d9ecefad94f5e79c3c43954370e97ddc155aa2
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-07-19 18:10:59 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
+import mongoose from "mongoose";
+
 import app from "./app.js";
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
-});
+const DB_HOST =
+  "mongodb+srv://Oleksandr:ULW4RCMD6ES0CP7y@cluster0.r82o1ri.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Database connection successful");
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
