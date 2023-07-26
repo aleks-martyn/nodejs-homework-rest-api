0692bf95034b57a0660ae644530a166e53c7f3d0
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-07-26 22:52:27 +0300

    Create users-schemas.js

---
+++ schemas/users-schemas.js
@@ -0,0 +1,9 @@
+import Joi from "joi";
+
+import { emailRegexp } from "../constants/user-constants.js";
+
+const userSignupSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().pattern(emailRegexp).required(),
+  password: Joi.string().min(6).required(),
+});
