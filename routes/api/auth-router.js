590225e94019f50eefc2ec75997b9fdd10fb6038
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-07-26 23:34:38 +0300

    Update auth-router.js

--- routes/api/auth-router.js
+++ routes/api/auth-router.js
@@ -2,6 +2,8 @@ import express from "express";
 
 import { validateBody } from "../../middlewares/index.js";
 
+import usersSchemas from "../../schemas/users-schemas.js";
+
 const authRouter = express.Router();
 
 export default authRouter;
