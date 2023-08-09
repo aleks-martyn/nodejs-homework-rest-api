9d4df36ce36e044b193a7b522ec89bb8e1ac69da
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-08-09 20:36:55 +0300

    Update sendEmail.js

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -1,13 +1,13 @@
 import nodemailer from "nodemailer";
 
-const { META_PASSWORD } = process.env;
+const { META_PASSWORD, META_EMAIL_FROM } = process.env;
 
 const nodemailerConfig = {
   host: "smtp.meta.ua",
   port: 465,
   secure: true,
   auth: {
-    user: "oleksandr.martyniuk@meta.ua",
+    user: META_EMAIL_FROM,
     pass: META_PASSWORD,
   },
 };
@@ -15,7 +15,7 @@ const nodemailerConfig = {
 const transport = nodemailer.createTransport(nodemailerConfig);
 
 const sendEmail = async (data) => {
-  const email = { ...data, from: "oleksandr.martyniuk@meta.ua" };
+  const email = { ...data, from: META_EMAIL_FROM };
   await transport.sendMail(email);
   return true;
 };
